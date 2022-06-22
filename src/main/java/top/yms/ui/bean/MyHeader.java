80c18392ebee2426957b672dcd30ab47027189a5
yangmingsen <yangmingsen16@outlook.com>
2022-06-22 11:00:57 +0800

    new updated

--- src/main/java/top/yms/ui/bean/MyHeader.java
+++ src/main/java/top/yms/ui/bean/MyHeader.java
@@ -1,6 +1,9 @@
 package top.yms.ui.bean;
 
-public class MyHeader {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyHeader implements BeanPostProcessor {
     private String name = "default-header";
 
     public String getName() {
@@ -9,5 +12,17 @@ public class MyHeader {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization>>>>MyHeader");
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization>>>>MyHeader");
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
     }
 }
--- src/test/java/top/yms/app/Test.java
+++ src/test/java/top/yms/app/Test.java
@@ -1,6 +1,16 @@
 package top.yms.app;
 
+import org.springframework.util.StringUtils;
+
 public class Test {
+
+    @org.junit.Test
+    public void test() {
+        String hh = "Hello,Hhh,osdf";
+        String[] strings = StringUtils.tokenizeToStringArray(hh, ",");
+        System.out.println(strings);
+
+    }
 
     public void test1() {
 
